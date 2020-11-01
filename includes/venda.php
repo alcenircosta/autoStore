bc5774a492f49062d8ac2601eeeb3a446fca3b9b
Alcenir Costa <alcenir.1994@gmail.com>
2020-11-01 17:05:24 -0200

    Removed header and footer from venda.php

--- includes/venda.php
+++ includes/venda.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
-	<title>Projeto 05</title>
-	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-	<meta charset="utf-8" />
-	<meta name="description" content="Front end website desenvolvido durante curso de programação"/>
-	<meta name="keywords" content="programação,desenvolvimento web, desenvolvimento front end,SEO"/>
-	<meta name="author" content="Alcenir da Costa"/>
-</head>
-<body>
 	<header style="border-bottom: 3px solid #EB2D2D;">
 		<div class="container">
 			<div class="logo">
@@ -143,21 +130,3 @@
 			</div><!--container-->
 		</div><!--content-venda-->
 	</section><!--venda-->
-	<footer>
-		<div class="container">
-			<nav>
-				<ul>
-					<li><a href="index.html">Home</a></li>
-					<li><a style="color:#EB2D2D;" href="venda.html">Venda</a></li>
-					<li><a href="sobre.html">Sobre</a></li>
-					<li><a href="">Contato</a></li>
-				</ul>
-			</nav>
-			<p>Todos os direitos reservados</p>
-			<div class="clear"></div>
-		</div><!--container-->
-	</footer>
-	<script src="js/jquery.js"></script>
-	<script src="js/functions.js"></script>
-</body>
-</html>
